7eb50f4b259bf854424e6140818d4b9fff26f9c3
Gopal Kaul <gopalkaul5@gmail.com>
2021-05-18 20:33:26 +0530

    Updates

---
+++ precache-manifest.65522a8f09f8dadcb40ab501a3c3715f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c8d30100f833cd73331",
+    "url": "/covid19-graph/static/css/main.12bb7053.chunk.css"
+  },
+  {
+    "revision": "7c8d30100f833cd73331",
+    "url": "/covid19-graph/static/js/main.98e5e958.chunk.js"
+  },
+  {
+    "revision": "1860fd08d92637eb9b6b",
+    "url": "/covid19-graph/static/js/runtime~main.2e31a040.js"
+  },
+  {
+    "revision": "beb370776d41f3902c83",
+    "url": "/covid19-graph/static/js/2.644d85f1.chunk.js"
+  },
+  {
+    "revision": "447f48571f66e7807be5",
+    "url": "/covid19-graph/static/js/3.d69099ea.chunk.js"
+  },
+  {
+    "revision": "493418ba3b11df46038500db06324990",
+    "url": "/covid19-graph/static/media/loading.493418ba.gif"
+  },
+  {
+    "revision": "4b6986cc4555e3e0ad3d33a8c0b432f4",
+    "url": "/covid19-graph/index.html"
+  }
+];
