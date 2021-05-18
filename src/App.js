6a8395a0e425b3ecc94287465c1ce5b2d6655564
Gopal Kaul <gopalkaul5@gmail.com>
2021-05-18 20:21:19 +0530

    Initial Commit. Completed Work.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Plot from "react-plotly.js";
+import loadingGIF from './loading.gif';
 
 function App() {
+  const [cases, setCases] = useState();
+  const [loading, setLoading]= useState(true);
+  const getData = () => {
+    axios
+      .get("https://api.covid19india.org/data.json")
+      .then((res) => res.data)
+      .then((res) => {
+        setCases(res["cases_time_series"]);
+        setLoading(false);
+      })
+      .catch((e) => console.log(`Error : ${e}`));
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ?<> <h1>Loading...</h1><img src={loadingGIF} alt="Loading Spinner GIF" /> </> : <></> }
+      {cases && (
+        <Plot
+          data={[
+            {
+              y: cases.map((x) => x["dailyconfirmed"]),
+              x: cases.map((x) => x["dateymd"]),
+              type: "scatter",
+              mode: "lines+markers",
+              marker: { color: "blue" },
+            },
+          ]}
+          layout={{
+            title: { text: "COVID Cases In India" },
+            xaxis: { title: "Date" },
+            yaxis: { title: "New Cases" },
+            autosize: true,
+            automargin: true,
+          }}
+          style={{ width: "100%", height: "100%" }}
+          config={{ responsive: true }}
+        />
+      )}
     </div>
   );
 }
